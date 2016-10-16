f73588e8cba78b79a1ad43793803a4d1e359961d
Daniel Sarmiento <d.sarmiento731@gmail.com>
2016-10-16 12:23:00 -0400

    Restared with blank activity

---
+++ DaftController/app/src/main/java/com/dsarmiento/daftcontroller/ControllerMain.java
@@ -0,0 +1,13 @@
+package com.dsarmiento.daftcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControllerMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controller_main);
+    }
+}

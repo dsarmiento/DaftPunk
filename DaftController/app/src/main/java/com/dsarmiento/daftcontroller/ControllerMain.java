2a6b3952226eb6467781d8942a3e0481798733ae
Daniel Sarmiento <d.sarmiento731@gmail.com>
2016-10-18 16:01:04 -0400

    Redid button layout to percentages, and added popup for bluetooth devices

--- DaftController/app/src/main/java/com/dsarmiento/daftcontroller/ControllerMain.java
+++ DaftController/app/src/main/java/com/dsarmiento/daftcontroller/ControllerMain.java
@@ -1,18 +1,31 @@
 package com.dsarmiento.daftcontroller;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.ListView;
 import android.widget.ToggleButton;
 
 import java.util.LinkedList;
-
+import java.util.Set;
 
 
 public class ControllerMain extends AppCompatActivity {
     private LinkedList<ToggleButton> toggleButtons;
+    private static int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter mBluetoothAdapter;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,9 +35,31 @@ public class ControllerMain extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_connect:
+                if(mBluetoothAdapter != null)
+                    connectBluetooth();
+                else
+                    testDialog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller_main);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter != null) {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
 
         toggleButtons = new LinkedList<>();
 
@@ -131,11 +166,51 @@ public class ControllerMain extends AppCompatActivity {
         });
     }
 
-    private void turnOff(ToggleButton toggle)
-    {
+    private void turnOff(ToggleButton toggle) {
         for(ToggleButton item : toggleButtons)
             if(!item.equals(toggle))
                 item.setChecked(false);
+    }
+
+    private void connectBluetooth() {
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(ControllerMain.this);
+            LayoutInflater inflater = getLayoutInflater();
+            View convertView = (View) inflater.inflate(R.layout.bt_list, null);
+            alertDialog.setView(convertView);
+            alertDialog.setTitle("Bluetooth Devices");
+
+            ListView lv = (ListView) convertView.findViewById(R.id.bt_listView);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+            for(BluetoothDevice device : pairedDevices) {
+                adapter.add(device.getName() + "\n" + device.getAddress());
+            }
+
+            lv.setAdapter(adapter);
+            alertDialog.show();
+        }
+    }
+
+    private void testDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(ControllerMain.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View convertView = (View) inflater.inflate(R.layout.bt_list, null);
+        alertDialog.setView(convertView);
+        alertDialog.setTitle("Test");
+
+        ListView lv = (ListView) convertView.findViewById(R.id.bt_listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+        for(int i = 0; i < 5; i++) {
+            char name = (char)('A' + i);
+            char addr = (char)('0' + i);
+            adapter.add(name + "\n" + addr);
+        }
+
+        lv.setAdapter(adapter);
+        alertDialog.show();
     }
 
 }
